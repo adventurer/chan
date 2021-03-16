101480902513996241b41791fa0d6a74394b2c2c
adventurer <1550520500@qq.com>
2021-03-16 23:25:45 +0800

    1

---
+++ auth/auth.go
@@ -0,0 +1,76 @@
+package auth
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type Result struct {
+	Sta  int
+	Code int
+	Msg  string
+	Data User
+}
+type User struct {
+	ID          int       `gorm:"AUTO_INCREMENT"`
+	OpenID      string    `gorm:"unique;size:255"` // string默认长度为255, 使用这种tag重设。
+	NickName    string    `gorm:"size:255"`
+	HeadImgURL  string    `gorm:"size:255"`
+	Password    string    `gorm:"size:255"`       // string默认长度为255, 使用这种tag重设。
+	Token       string    `gorm:"index;size:255"` // string默认长度为255, 使用这种tag重设。
+	LastLogin   time.Time `gorm:"default:null"`
+	Status      int       `gorm:"default:1"` // 1未激活2已激活3封禁
+	ExpiredAt   time.Time `gorm:"default:null"`
+	ExpiredAtTs int64     `gorm:"-"` //ignore this field
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
+}
+
+var cacheTime time.Time = time.Now()
+
+func Auth(apiAddress, token string) bool {
+	defer recoverName()
+	if cacheTime.Unix() > time.Now().Unix() {
+		log.Println("auth cache")
+		return true
+	}
+	resp, err := http.PostForm(apiAddress,
+		url.Values{"token": {token}})
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		// handle error
+		log.Println(err)
+	}
+	result := Result{}
+	err = json.Unmarshal(body, &result)
+	if err != nil {
+		log.Printf("err on http:%#v", err)
+		return false
+	}
+	if result.Sta == 0 {
+		return false
+	}
+	if result.Data.ExpiredAt.Unix() < time.Now().Unix() {
+		return false
+	}
+	log.Println("auth http")
+	cacheTime = result.Data.ExpiredAt
+	return true
+}
+
+func recoverName() {
+	if r := recover(); r != nil {
+		fmt.Println("recovered from Login:", r)
+	}
+}
